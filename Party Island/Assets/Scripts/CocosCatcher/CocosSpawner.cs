efbeef818b2f139bfba2f3fd09a75ba088a869fe
StanVDL <92679342+StanVDL@users.noreply.github.com>
2022-04-04 11:16:14 +0200

    Started with CocosCatcher minigame

---
+++ Party Island/Assets/Scripts/CocosCatcher/CocosSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CocosSpawner : MonoBehaviour
+{
+    public GameObject Cocos;
+    public GameObject BadCocos;
+
+    public float xBounds, yBound;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRandomCocos());
+    }
+
+    IEnumerator SpawnRandomCocos()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 2));
+
+        int randomCocos = Random.Range(0, 1);
+
+        if (Random.value <= 0.6f)
+        {
+            Instantiate(Cocos, new Vector3(Random.Range(-xBounds, xBounds), yBound), Quaternion.identity);
+        }
+
+        else
+        {
+            Instantiate(BadCocos, new Vector3(Random.Range(-xBounds, xBounds), yBound), Quaternion.identity);
+        }
+
+        StartCoroutine(SpawnRandomCocos());
+    }
+    
+}
