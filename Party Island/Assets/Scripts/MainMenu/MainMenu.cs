f63fe92ba862a5758c742555b913bb1343c5f233
StanVDL <92679342+StanVDL@users.noreply.github.com>
2022-04-13 13:33:33 +0200

    Save settings added

--- Party Island/Assets/Scripts/MainMenu/MainMenu.cs
+++ Party Island/Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public GameObject OptionsMenus;
+    public GameObject MainMenus;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Island");
     }
 
+    public void OptionsMenu()
+    {
+        OptionsMenus.SetActive(true);
+        MainMenus.SetActive(false);
+
+    }
 }
--- Party Island/Assets/Scripts/MainMenu/SettingsMenu.cs
+++ Party Island/Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -1,11 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public GameObject OptionsMenu;
+    public GameObject MainMenu;
+
+    public TMPro.TMP_Dropdown qualityDropdown;
+
+    const string prefName = "optionsvalue";
+
+    void Awake()
+    {
+        qualityDropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
+            {
+                PlayerPrefs.SetInt(prefName, qualityDropdown.value);
+            }));
+    }
+
+    void Start()
+    {
+        qualityDropdown.value = PlayerPrefs.GetInt(prefName, 3);
+    }
+
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void BackToMainMenu()
+    {
+        OptionsMenu.SetActive(false);
+        MainMenu.SetActive(true);
     }
 }
